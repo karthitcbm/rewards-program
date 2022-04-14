1cb663d8af76f25e52f5bae3d447d0ddc590f5e0
karthik <karthi.tcbm@gmail.com>
2022-04-14 15:22:18 -0500

    rewards program base code

---
+++ src/data.js
@@ -0,0 +1,36 @@
+module.exports = {
+  john: [
+    {
+      date: "1/1/2020",
+      amount: 100
+    },
+    {
+      date: "2/1/2020",
+      amount: 10
+    }
+  ],
+  paul: [
+    {
+      date: "1/1/2020",
+      amount: 100
+    },
+    {
+      date: "2/28/2020",
+      amount: 140
+    }
+  ],
+  steve: [
+    {
+      date: "2/1/2020",
+      amount: 2354
+    },
+    {
+      date: "1/26/2020",
+      amount: 120
+    }
+  ],
+  mary: [
+
+  ]
+};
+
